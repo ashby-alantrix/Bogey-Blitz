6a061582efd504cd996bbdd835fe158416abb2ed
Ashby Issac <ashby@alantrixgeim.com>
2025-12-15 20:31:07 +0530

    Refactoring + env design.
    Redesigned the environment prefab.
    Added a new stopper obstacle.
    Refactored prefabs.

--- Assets/Scripts/Constants/BogeyBlitz_Constants.cs
+++ Assets/Scripts/Constants/BogeyBlitz_Constants.cs
@@ -3,4 +3,5 @@ public class BogeyBlitz_Constants
 {
     public const string STRAIGHT_TRACK_TAG = "Straight-Track";  
     public const string CURVED_TRACK_TAG = "Curved-Track";  
+    public const string OBSTACLE_TAG = "Obstacle";  
 }
